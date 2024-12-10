c57f34495426a902dcbc48393f9f34dacb309de3
Nakayama Haruto <e245757@ie.u-ryukyu.ac.jp>
2024-12-10 16:43:27 +0900

    try-catch構文を追加

--- ExceptionHandling.java
+++ ExceptionHandling.java
@@ -1,6 +1,13 @@
 public class ExceptionHandling {
         public static void main(String[] args) {
             String str = null;
-            str.length();
+            try {
+                str.length();
+            } catch (NullPointerException e) {
+                // TODO: handle exception
+                System.out.println("エラーが発生しました: " + e.getMessage());
+            }
+            
+            
     }
 }
